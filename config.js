7d1895fc3cc10d6863784cb79f1b035a42930fb3
mintkunch <kuryu092607@gmail.com>
2024-10-12 11:16:18 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const app_id = "ココに記入" //skywayのアプリケーションID //skyway application ID
-const secret_key = "ココに記入" //skywayのシークレットキー // skyway secret key
+const app_id = "b4482a0e-f42e-4e72-935a-a061fe405c4b" //skywayのアプリケーションID //skyway application ID
+const secret_key = "vgPl2zlS5FqJ0qD2YhqM2IoSNwF2kYEChQ7qoEF9mvc=" //skywayのシークレットキー // skyway secret key
 let proximity = true;  // 近接VCのtrue:有効/false:無効(デフォルトはtrue) //Proximity VC true:enable/false:disable (default: true)
 let distance = 6; // 声の届く最大距離(デフォルトは6) //Max distance of sound (default: 6)
 const port = 19132; // websocketをlistenするポート //Port to listen
